351f4744bfd71833160771fd40765e6c2737b119
thijsdenbutter <thijs@cncware.nl>
2025-10-18 16:27:39 +0200

    notification repository gemaakt

---
+++ src/main/java/cncware/cncwareserviceportalbackend/repositories/NotificationRepository.java
@@ -0,0 +1,6 @@
+package cncware.cncwareserviceportalbackend.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<NotificationRepository, Integer> {
+}
