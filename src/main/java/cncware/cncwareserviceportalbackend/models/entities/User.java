d3bfdea31f0ada653eb20527b903752520b4e845
thijsdenbutter <thijs@cncware.nl>
2025-10-18 14:54:02 +0200

    user entiteit zonder relaties aangemaakt

--- src/main/java/cncware/cncwareserviceportalbackend/models/entities/User.java
+++ src/main/java/cncware/cncwareserviceportalbackend/models/entities/User.java
@@ -1,8 +1,19 @@
 package cncware.cncwareserviceportalbackend.models.entities;
 
 import cncware.cncwareserviceportalbackend.models.enums.Role;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue
     private int id;
     private String email;
     private String password;
