1de5c4720d0466ab864a38f580c7a56bf35682de
thijsdenbutter <thijs@cncware.nl>
2025-10-19 16:51:50 +0200

    Message dto's gemaakt

---
+++ src/main/java/cncware/cncwareserviceportalbackend/dtos/output/MessageOutputDto.java
@@ -0,0 +1,15 @@
+package cncware.cncwareserviceportalbackend.dtos.output;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+public class MessageOutputDto {
+
+    private int id;
+    private String content;
+    private LocalDateTime timestamp;
+}
