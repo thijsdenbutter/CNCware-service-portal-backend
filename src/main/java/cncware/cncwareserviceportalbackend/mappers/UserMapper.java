cc1fb1638613b64608d9750b508e48019b87299e
thijsdenbutter <thijs@cncware.nl>
2025-10-23 19:38:17 +0200

    User mapper gemaakt

---
+++ src/main/java/cncware/cncwareserviceportalbackend/mappers/UserMapper.java
@@ -0,0 +1,40 @@
+package cncware.cncwareserviceportalbackend.mappers;
+
+import cncware.cncwareserviceportalbackend.dtos.input.UserInputDto;
+import cncware.cncwareserviceportalbackend.dtos.list.UserListDto;
+import cncware.cncwareserviceportalbackend.dtos.output.UserOutputDto;
+import cncware.cncwareserviceportalbackend.models.entities.Notification;
+import cncware.cncwareserviceportalbackend.models.entities.Ticket;
+import cncware.cncwareserviceportalbackend.models.entities.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    @Mapping(target = "companyId", source = "company.id")
+    @Mapping(target = "ticketIds", source = "tickets")
+    @Mapping(target = "notificationIds", source = "notifications")
+    UserOutputDto toDto(User entity);
+
+    UserListDto toListDto(User entity);
+
+    User toEntity(UserInputDto dto);
+
+    void updateEntity(UserInputDto dto, @MappingTarget User entity);
+
+    List<UserListDto> toDtoList(List<User> entities);
+
+    default List<Integer> getTicketIds (List<Ticket> tickets){
+        if (tickets == null) return null;
+        return tickets.stream().map(Ticket::getId).toList();
+    }
+
+    default List<Integer> getNotificationIds (List<Notification> notifications){
+        if (notifications == null) return null;
+        return notifications.stream().map(Notification::getId).toList();
+    }
+}
