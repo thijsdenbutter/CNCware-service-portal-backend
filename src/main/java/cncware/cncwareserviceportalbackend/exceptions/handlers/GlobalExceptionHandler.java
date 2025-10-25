2fec8d19d775a9815bd450674779a1eb580dc209
thijsdenbutter <thijs@cncware.nl>
2025-10-25 14:52:45 +0200

    resource not found handler in global handler gemaakt

--- src/main/java/cncware/cncwareserviceportalbackend/exceptions/handlers/GlobalExceptionHandler.java
+++ src/main/java/cncware/cncwareserviceportalbackend/exceptions/handlers/GlobalExceptionHandler.java
@@ -1,4 +1,21 @@
 package cncware.cncwareserviceportalbackend.exceptions.handlers;
 
-public class GlabalExeptionHandler {
+import cncware.cncwareserviceportalbackend.exceptions.ApiError;
+import cncware.cncwareserviceportalbackend.exceptions.types.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = ResourceNotFoundException.class)
+    public ResponseEntity<ApiError> resourceNotFoundHandler(ResourceNotFoundException exception, HttpServletRequest request){
+        ApiError apiError = new ApiError(HttpStatus.NOT_FOUND, exception.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
+    }
+
 }
