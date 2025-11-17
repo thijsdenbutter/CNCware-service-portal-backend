07c5a9eb7b17c95064f34ca239113842926aa3b1
thijsdenbutter <thijs@cncware.nl>
2025-11-17 21:09:01 +0100

    Method not allowed + endpoint not found handler toegevoegd

--- src/main/java/cncware/cncwareserviceportalbackend/exceptions/handlers/GlobalExceptionHandler.java
+++ src/main/java/cncware/cncwareserviceportalbackend/exceptions/handlers/GlobalExceptionHandler.java
@@ -3,15 +3,15 @@ package cncware.cncwareserviceportalbackend.exceptions.handlers;
 import cncware.cncwareserviceportalbackend.exceptions.ApiError;
 import cncware.cncwareserviceportalbackend.exceptions.types.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.ValidationException;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.stream.Collectors;
 
@@ -19,14 +19,18 @@ import java.util.stream.Collectors;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = ResourceNotFoundException.class)
-    public ResponseEntity<ApiError> resourceNotFoundHandler(ResourceNotFoundException exception, HttpServletRequest request){
+    public ResponseEntity<ApiError> resourceNotFoundHandler(ResourceNotFoundException exception,
+            HttpServletRequest request){
+
         ApiError error = new ApiError(HttpStatus.NOT_FOUND, exception.getMessage(), request.getRequestURI());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiError> validationHandler(MethodArgumentNotValidException exception, HttpServletRequest request){
+    public ResponseEntity<ApiError> validationHandler(MethodArgumentNotValidException exception ,
+            HttpServletRequest request){
+
         String errorMessage = exception
                 .getBindingResult()
                 .getFieldErrors()
@@ -40,9 +44,8 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<ApiError> handleNotReadable(
-            HttpMessageNotReadableException ex,
-            HttpServletRequest request) {
+    public ResponseEntity<ApiError> handleNotReadable(HttpMessageNotReadableException exception,
+                                                      HttpServletRequest request){
 
         ApiError error = new ApiError(
                 HttpStatus.BAD_REQUEST,
@@ -52,9 +55,43 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ApiError> handleMethodNotSupported(HttpRequestMethodNotSupportedException exception,
+            HttpServletRequest request){
+
+        String message = String.format(
+                "HTTP method '%s' is not supported for this endpoint.",
+                exception.getMethod()
+        );
+
+        ApiError error = new ApiError(
+                HttpStatus.METHOD_NOT_ALLOWED,
+                message,
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(error);
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ApiError> handleNoResource(NoResourceFoundException exception,
+            HttpServletRequest request){
+
+        ApiError error = new ApiError(
+                HttpStatus.NOT_FOUND,
+                "Endpoint not found. The requested path does not exist.",
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiError> handleUnexpected(Exception exception, HttpServletRequest request) {
+    public ResponseEntity<ApiError> handleUnexpected(Exception exception
+            , HttpServletRequest request){
+
+        exception.printStackTrace();
+
         ApiError error = new ApiError(
                 HttpStatus.INTERNAL_SERVER_ERROR,
                 "An unexpected error occurred.",
