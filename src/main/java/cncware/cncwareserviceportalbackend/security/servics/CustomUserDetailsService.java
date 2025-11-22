4d8ca773ec8fdeb1859747cbd08b2c3135fb4cd8
thijsdenbutter <thijs@cncware.nl>
2025-11-22 16:32:46 +0100

    CustomUserDetailsService aangemaakt + validatie ingebouwd voor uniek email adres in User service

--- src/main/java/cncware/cncwareserviceportalbackend/security/servics/CustomUserDetailsService.java
+++ src/main/java/cncware/cncwareserviceportalbackend/security/servics/CustomUserDetailsService.java
@@ -1,4 +1,28 @@
 package cncware.cncwareserviceportalbackend.security.servics;
 
-public class CustomUserDetailsService {
+import cncware.cncwareserviceportalbackend.models.entities.User;
+import cncware.cncwareserviceportalbackend.repositories.UserRepository;
+import cncware.cncwareserviceportalbackend.security.models.UserPrincipal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User with email '" + email + "' not found")
+                );
+
+        return new UserPrincipal(user);
+    }
 }
