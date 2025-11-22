199dc51f8a12cd13e12b55115f73d463b5729f33
thijsdenbutter <thijs@cncware.nl>
2025-11-22 14:03:59 +0100

    SecurityConfig basis aangemaakt

--- src/main/java/cncware/cncwareserviceportalbackend/security/config/SecurityConfig.java
+++ src/main/java/cncware/cncwareserviceportalbackend/security/config/SecurityConfig.java
@@ -1,4 +1,29 @@
 package cncware.cncwareserviceportalbackend.security.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/error").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
 }
