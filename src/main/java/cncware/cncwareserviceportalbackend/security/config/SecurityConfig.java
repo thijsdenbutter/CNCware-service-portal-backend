ce7cb08144e4f6b92a1622c1418a438c921498df
thijsdenbutter <thijs@cncware.nl>
2025-11-22 20:57:41 +0100

    SecurityConfig uitgebreid met endpoint beveiliging

--- src/main/java/cncware/cncwareserviceportalbackend/security/config/SecurityConfig.java
+++ src/main/java/cncware/cncwareserviceportalbackend/security/config/SecurityConfig.java
@@ -1,16 +1,26 @@
 package cncware.cncwareserviceportalbackend.security.config;
 
+import cncware.cncwareserviceportalbackend.security.jwt.JwtAuthenticationFilter;
+import cncware.cncwareserviceportalbackend.security.servics.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomUserDetailsService customUserDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -21,9 +31,44 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/error").permitAll()
-                        .anyRequest().authenticated()
+
+                        .requestMatchers("/users/**").hasRole("ADMIN")
+                        .requestMatchers("/companies/**").hasRole("ADMIN")
+
+                        .requestMatchers("/statuses/**").hasAnyRole("ADMIN", "USER", "CONSULTANT")
+
+                        .requestMatchers("GET", "/tickets/**").hasAnyRole("ADMIN", "USER", "CONSULTANT")
+                        .requestMatchers("POST", "/tickets").hasAnyRole("ADMIN", "USER", "CONSULTANT")
+                        .requestMatchers("PUT", "/tickets/**").hasAnyRole("ADMIN", "CONSULTANT")
+                        .requestMatchers("DELETE", "/tickets/**").hasAnyRole("ADMIN", "CONSULTANT")
+
+                        .requestMatchers("POST", "/messages/**").hasAnyRole("ADMIN", "USER", "CONSULTANT")
+                        .requestMatchers("DELETE", "/messages/**").hasAnyRole("ADMIN", "CONSULTANT")
+
+                        .requestMatchers("GET", "/timers/**").hasAnyRole("ADMIN", "USER", "CONSULTANT")
+                        .requestMatchers("POST", "/timers").hasAnyRole("USER", "CONSULTANT")
+                        .requestMatchers("PUT", "/timers/**").hasAnyRole("USER", "CONSULTANT")
+                        .requestMatchers("DELETE", "/timers/**").hasAnyRole("ADMIN", "CONSULTANT")
+
+                        .requestMatchers("GET", "/notifications/**").hasAnyRole("ADMIN", "USER", "CONSULTANT")
+                        .requestMatchers("POST", "/notifications").hasAnyRole("ADMIN", "CONSULTANT")
+                        .requestMatchers("DELETE", "/notifications/**").hasRole("ADMIN")
+
+                        .anyRequest().denyAll()
                 )
-                .httpBasic(Customizer.withDefaults());
+                .userDetailsService(customUserDetailsService)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
